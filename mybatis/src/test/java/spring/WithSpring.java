dc4bd4c83d0e9e6c5cefa530d42de5a8eb31d9f9
loveverdl <loveverdl@gmail.com>
2013-06-06 09:47:58 +0800

    2

--- mybatis/src/test/java/spring/WithSpring.java
+++ mybatis/src/test/java/spring/WithSpring.java
@@ -1,5 +1,7 @@
 package spring;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import test.dao.KindMapper;
@@ -12,7 +14,9 @@ public class WithSpring {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
-		KindMapper km = (KindMapper) context.getBean("kindMapper");
+		SqlSessionFactory fact = (SqlSessionFactory)context.getBean("sqlSessionFactoryMgr");
+		SqlSession session = fact.openSession();
+		KindMapper km = session.getMapper(KindMapper.class);
 		System.out.println(km.selectByPrimaryKey(10002).getKind_name());
 	}
 
